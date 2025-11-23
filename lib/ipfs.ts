34ec0519a548286904900a64fb37f257c8fca45b
nickthelegend <testingtesla7@gmail.com>
2025-11-23 08:33:54 +0530

    Changing IPFS provider to pinata

--- lib/ipfs.ts
+++ lib/ipfs.ts
@@ -2,7 +2,7 @@ export function convertIpfsUrl(ipfsUrl: string): string {
   if (ipfsUrl.startsWith('ipfs://')) {
     const cid = ipfsUrl.replace('ipfs://', '')
     // Using Cloudflare's IPFS gateway for better reliability and speed
-    return `https://cloudflare-ipfs.com/ipfs/${cid}`
+    return `https://white-holy-caterpillar-795.mypinata.cloud/ipfs/${cid}`
   }
   return ipfsUrl
 }
